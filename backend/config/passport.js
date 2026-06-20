cbe840e6d4785c1aa7d167359301ff91dbfbb9ac
kruthikajakkidi <kruthikajakkidi@gmail.com>
2026-06-20 18:14:00 +0530

    Refactor GoogleStrategy callback and user creation

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -8,29 +8,26 @@ export default function configurePassport() {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL:
-          process.env.GOOGLE_CALLBACK_URL ||
-          `${process.env.SERVER_URL || `http://localhost:${process.env.PORT || 5000}`}/auth/google/callback`
+        callbackURL: process.env.GOOGLE_CALLBACK_URL
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
           const email = profile.emails?.[0]?.value;
+
           let userDocument = await UserModel.findOne({
             $or: [{ googleId: profile.id }, { email }]
           });
 
           if (!userDocument) {
             userDocument = await UserModel.create({
-              firstName: profile.name?.givenName || "ANVIA",
-              lastName: profile.name?.familyName || "Member",
+              firstName: profile.name?.givenName || "User",
+              lastName: profile.name?.familyName || "ANVIA",
               email,
               googleId: profile.id,
-              profileImage: profile.photos?.[0]?.value
+              profileImage: profile.photos?.[0]?.value || ""
             });
           } else if (!userDocument.googleId) {
             userDocument.googleId = profile.id;
-            userDocument.profileImage =
-              userDocument.profileImage || profile.photos?.[0]?.value || "";
             await userDocument.save();
           }
 
