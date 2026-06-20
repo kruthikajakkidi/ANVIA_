b08381e1926a7860da0ef052913c0e86d7e6af2b
kruthikajakkidi <kruthikajakkidi@gmail.com>
2026-06-20 19:20:38 +0530

    Enhance Google OAuth callback URL handling

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -2,32 +2,44 @@ import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import UserModel from "../models/UserModel.js";
 
+function getServerUrl() {
+  return (
+    process.env.SERVER_URL ||
+    process.env.RENDER_EXTERNAL_URL ||
+    `http://localhost:${process.env.PORT || 5000}`
+  ).replace(/\/+$/, "");
+}
+
 export default function configurePassport() {
   passport.use(
     new GoogleStrategy(
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: process.env.GOOGLE_CALLBACK_URL
+        callbackURL:
+          process.env.GOOGLE_CALLBACK_URL ||
+          `${getServerUrl()}/auth/google/callback`,
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
           const email = profile.emails?.[0]?.value;
 
           let userDocument = await UserModel.findOne({
-            $or: [{ googleId: profile.id }, { email }]
+            $or: [{ googleId: profile.id }, { email }],
           });
 
           if (!userDocument) {
             userDocument = await UserModel.create({
-              firstName: profile.name?.givenName || "User",
-              lastName: profile.name?.familyName || "ANVIA",
+              firstName: profile.name?.givenName || "ANVIA",
+              lastName: profile.name?.familyName || "Member",
               email,
               googleId: profile.id,
-              profileImage: profile.photos?.[0]?.value || ""
+              profileImage: profile.photos?.[0]?.value,
             });
           } else if (!userDocument.googleId) {
             userDocument.googleId = profile.id;
+            userDocument.profileImage =
+              userDocument.profileImage || profile.photos?.[0]?.value || "";
             await userDocument.save();
           }
 
